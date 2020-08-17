2c7b5bb2530ae8da920e7073e9c3a670d7279399
angelliambo <aliambo@navent.com>
2020-08-17 16:32:41 -0300

    serverworker creacion exitosa

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
-{/**
-  *Mediante Axios realizamos una consulta tipo asincronica a un server publico del clima, 
-  solo necesitamos la url y los parametros con los datos que esperan en la consulta
- */}
+/**
+  * Mediante Axios realizamos una consulta tipo asincronica a un server publico del clima, 
+  * solo necesitamos la url y los parametros con los datos que esperan en la consulta
+ */
 
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
