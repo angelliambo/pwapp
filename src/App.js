15b983a93e5a66ebea22aa174111f020ddebcf94
angelliambo <aliambo@navent.com>
2020-08-17 16:11:02 -0300

    Creados todos los elementos basico para mostrar info al usuario

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css'
 
 const App = () => {
   const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
   const search = async (e) => {
+
+    // Comenzamos la busqueda luego que detectamos que la tecla presionada por el usuario es Enter 
     if ( e.key === 'Enter'){
+      // La info obtenida en la api la seteamos como el nuevo clima
       const data = await fetchWeather(query)
     
-      console.log(data)
+      setWeather(data)
+      setQuery('')
     }
   }
 
@@ -19,11 +24,29 @@ const App = () => {
     <input
     type='text'
     className='search'
-    placeholder='Serch...'
+    placeholder='Search...'
     value={query}
     onChange={(e)=> setQuery(e.target.value)}
     onKeyPress={search}
      />
+    
+    {weather.main && (
+      <div className="city">
+        <h2 className="city-name">
+          <span>{weather.name}</span>
+          <sup>{weather.sys.country}</sup>
+        </h2>
+        <div className="city-temp" >
+          {Math.round(weather.main.temp)}
+          <sup>&deg;C</sup>
+        </div>
+        <div className="info">
+        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+        <p>{weather.weather[0].description}</p>
+        </div>
+      </div>
+    )}
+
 
     </div>
   );
